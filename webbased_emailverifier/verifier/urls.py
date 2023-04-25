efa5283e56cff66289240dfbcada92e2ac3a2cf0
nick-TAG <nick@tag-cyber.com>
2023-04-25 11:15:11 -0400

    first commit

---
+++ webbased_emailverifier/verifier/urls.py
@@ -0,0 +1,11 @@
+#from the current folder import views
+from . import views
+#importing path from django.urls
+from django.urls import path
+
+#this is the list of the app's views
+#if the app has several views then it will have serveral paths
+
+urlpatterns = [
+    path('', views.index, name='home')
+]
